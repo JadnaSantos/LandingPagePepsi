312dd9a048d43f72227519cb5831c6a9ef236ea3
Jadna Silva <62259770+jadblue@users.noreplay.github.com>
2020-09-19 22:02:23 -0300

    landingPage

---
+++ script.js
@@ -0,0 +1,8 @@
+function slider(anything){
+    document.querySelector('.pepsi').src =anything;
+}
+
+function changeColor(color){
+    const rIndex = document.querySelector('.rIndex');
+    rIndex.style.background = color;
+}
